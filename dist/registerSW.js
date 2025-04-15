3afe88e62ed6fba010f0422fe05b64e184a281f4
Harun GÖREN <gorenharun22@gmail.com>
2025-04-15 18:09:20 +0300

    Fix asset paths and security headers

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/blackgoren.github.io/sw.js', { scope: '/blackgoren.github.io/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
